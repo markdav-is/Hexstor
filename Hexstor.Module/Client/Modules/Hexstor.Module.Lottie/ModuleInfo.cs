af392eb23ca0500b6bfc1b1baf0c73ed2632999a
Mark Davis <github@nohorse.com>
2025-01-13 18:18:27 -0800

    Add Lottie module with settings and display logic
    - Introduced a new module for displaying Lottie animations.
    - Created main component to load and show Lottie files.
    - Added settings page for configuring the Lottie resource URL.
    - Implemented property change handling to update animations dynamically.

---
+++ Hexstor.Module/Client/Modules/Hexstor.Module.Lottie/ModuleInfo.cs
@@ -0,0 +1,29 @@
+using Oqtane.Models;
+using Oqtane.Modules;
+using Oqtane.Shared;
+using System.Collections.Generic;
+
+
+namespace Hexstor.Module.Lottie
+{
+    public class ModuleInfo : IModule
+    {
+        public ModuleDefinition ModuleDefinition => new ModuleDefinition
+        {
+            Name = "Lottie",
+            Description = "plays lotties",
+            Version = "1.0.0",
+            ServerManagerType = "Trailblazor.Module.Lottie.Manager.LottieManager, Trailblazor.Module.Lottie.Server.Oqtane",
+            ReleaseVersions = "1.0.0",
+            Dependencies = "Trailblazor.Module.Lottie.Shared.Oqtane",
+            PackageName = "Trailblazor.Module.Lottie",
+            Resources = new List<Resource>()
+            {
+                //load the lottie-web component library
+                new Resource { ResourceType = ResourceType.Script,
+                    ES6Module=true, Location=ResourceLocation.Body,
+                    Url = "https://unpkg.com/@lottiefiles/dotlottie-wc@latest/dist/dotlottie-wc.js" }
+            }
+        };
+    }
+}
