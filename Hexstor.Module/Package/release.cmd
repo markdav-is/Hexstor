6817303feca699dd89088be2d65fc2ece0289605
Mark Davis <github@nohorse.com>
2025-01-19 09:24:58 -0800

    Update package files for net9.0 compatibility
    - Changed target framework from net8.0 to net9.0 for multiple DLLs and PDBs.
    - Added new ink_compiler.dll and ink-engine-runtime.dll files.
    - Updated the release script to use REM instead of REN for clarity.

--- Hexstor.Module/Package/release.cmd
+++ Hexstor.Module/Package/release.cmd
@@ -1,4 +1,4 @@
-REN  Hexstor.Module.Template.nuspec.REMOVE Hexstor.Module.Template.nuspec 
-"..\..\oqtane.framework\oqtane.package\nuget.exe" pack Hexstor.Module.Template.nuspec 
+REM  Hexstor.Module.Template.nuspec.REMOVE Hexstor.Module.Template.nuspec 
+"..\..\oqtane.framework\oqtane.package\nuget.exe" pack Hexstor.Module.nuspec 
 XCOPY "*.nupkg" "..\..\oqtane.framework\Oqtane.Server\Packages\" /Y
 
