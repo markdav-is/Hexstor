dd7b6bb551da227dc8158cb77436eacc14e9233a
markdav-is <github@nohorse.com>
2025-01-21 13:25:26 -0800

    Bump version to 1.0.1 across modules
    Updated version numbers from 1.0.0 to 1.0.1 in multiple modules and project files, ensuring consistency across the application.

--- Hexstor.Module/Client/Modules/Hexstor.Module.Lottie/ModuleInfo.cs
+++ Hexstor.Module/Client/Modules/Hexstor.Module.Lottie/ModuleInfo.cs
@@ -12,9 +12,9 @@ namespace Hexstor.Module.Lottie
         {
             Name = "Lottie",
             Description = "plays lotties",
-            Version = "1.0.0",
+            Version = "1.0.1",
             ServerManagerType = "Trailblazor.Module.Lottie.Manager.LottieManager, Trailblazor.Module.Lottie.Server.Oqtane",
-            ReleaseVersions = "1.0.0",
+            ReleaseVersions = "1.0.1",
             Dependencies = "Trailblazor.Module.Lottie.Shared.Oqtane",
             PackageName = "Trailblazor.Module.Lottie",
             Resources = new List<Resource>()
