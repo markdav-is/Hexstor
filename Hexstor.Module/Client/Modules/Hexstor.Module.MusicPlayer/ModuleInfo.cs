510d28f6160f241f26c5de018523dc809f05b73e
Joel Southall <vextechmage@gmail.com>
2025-01-26 05:15:11 -0800

    WIP: Broken branch with unresolved Oqtane.Shared issues and music player module errors

---
+++ Hexstor.Module/Client/Modules/Hexstor.Module.MusicPlayer/ModuleInfo.cs
@@ -0,0 +1,16 @@
+﻿using Oqtane.Models;
+using Oqtane.Modules;
+
+namespace Hexstor.Module.MusicPlayer
+{
+    public class ModuleInfo : IModule
+    {
+        public ModuleDefinition ModuleDefinition => new ModuleDefinition
+        {
+            Name = "MusicPlayer",
+            Description = "A module for playing background music.",
+            Version = "1.0.0",
+            PackageName = "Hexstor.Module.MusicPlayer"
+        };
+    }
+}
